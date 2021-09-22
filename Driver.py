39fb00d6b059c3e8b2726d3b8da4f93096fb0a75
matthew grewe <MatthewGrewe1@cu-biotedu-2-10.235.139.151.int.colorado.edu>
2021-09-22 10:22:22 -0600

    Created Driver file

---
+++ Driver.py
@@ -0,0 +1,9 @@
+
+
+if name == "__main__":
+    enviornment = Enviornment.enviornment()
+    enviornment.reset()
+    Steps = 45
+    for i in range(Steps):
+        enviornment.step()
+    States.enviornment.states.state_history
