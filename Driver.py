6f76b4810f8bf34bbd4744c2bc646bd494056178
matthew grewe <MatthewGrewe1@matthews-MacBook-Pro-2.local>
2021-10-09 15:24:10 -0600

    Changed the name and added in the information that Adam asked me to

--- Driver.py
+++ Driver.py
@@ -1,5 +1,5 @@
-import PursuitEvasionEnviornment #Connects the enviornment file (class)
-import PursuitEvasionSimulator #Connects the Simulator file (class)
+import SpaceCraftTaskingEnviornment #Connects the enviornment file (class)
+import SpaceCraftTaskingSimulator #Connects the Simulator file (class)
 import numpy as np #This is the package for using the array
 import matplotlib.pyplot as plt #This is the package for plotting
 import timeit as timer #This is the package for timing how long the code is runnign
@@ -8,17 +8,15 @@ import math as m
 
 #Add a space craft to the state vector
 
-#name = "__main__" #What are we looking at name for?
-
 if __name__ == "__main__": #What are we using this for?
-    enviornment = PursuitEvasionEnviornment.Enviornment() #Initalizes the enviornment
+    enviornment = SpaceCraftTaskingEnviornment.Enviornment() #Initalizes the enviornment
     r = 1.12639*1.4959787e11
     mu = 1.327124e20 #(m^3/s^2)mu of the sun
     v = m.sqrt(mu/r)
-    inital_conditions = [0, r, 0, -v, 0, 0, 1000, (1000 + r), 1000, 0, 0, 0] #Bennu orbiting around the sun
+    inital_conditions = np.array([0, r, 0, -v, 0, 0, 1000, 1000, 1000, 0, 0, 0]) #Bennu orbiting around the sun
         #(0-5 Bennu state vector, 6-11 spacecraft state vectore)
     enviornment.reset(inital_conditions) #Resets the enviornment
-    Steps = 1000 #Number of times to run through the enviornment
+    Steps = 5000 #Number of times to run through the enviornment
 
     histArray = np.zeros((12, Steps)) #Preallocates an array to hold the history data
 
@@ -39,7 +37,9 @@ if __name__ == "__main__": #What are we using this for?
 
     ax.plot3D(histArray[0,:], histArray[1,:], histArray[2,:], color="red", label="Bennu's Orbit")
         #Plots the orbit of Bennu about the sun
-    ax.plot3D(histArray[6,:], histArray[7,:], histArray[8,:], color="green", label="Spacecraft around Bennu")
+
+    ax.plot3D(histArray[0,:] - histArray[6,:], histArray[1,:] - histArray[7,:], histArray[2,:] - histArray[8,:], color="green", label="Spacecraft around Bennu")
         #Plots the orbit of the spacecraft about Bennu
 
+    ax.ticklabel_format(useOffset = False)
     plt.show() #Shows the plot
